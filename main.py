bc4333f9d9bffc33422a0a93047a3c25e78e4854
jlbrown5-ncat <60116362+jlbrown5-ncat@users.noreply.github.com>
2021-12-12 10:32:39 -0500

    Update Broker

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ import random
 if __name__ == '__main__':
     # This is the Publisher
     wait_interval = 30
+    BROKER_URL = "broker.mqttdashboard.com"
+    BROKER_PORT = 1883
 
     while True:
         status = random.randint(0, 2)
 
         client = mqtt.Client()
-        client.connect("192.168.1.59", 1883, 60)
+        client.connect(BROKER_URL, BROKER_PORT)
         # client.publish("topic/test", "Hello world!")
         client.publish("traffic", status)
         client.disconnect()
